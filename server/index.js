c2d733b964161ea6a85c8439797a334382fdab84
dipesh7562 <kdipesh2004@gmail.com>
2025-07-27 14:33:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express= require('express');
 const mongoose= require('mongoose');
-const port=process.env.PORT || 3001;
+const port='https://myclient-a7xx.onrender.com'
 const app= express();
 const cors=require('cors')
 
